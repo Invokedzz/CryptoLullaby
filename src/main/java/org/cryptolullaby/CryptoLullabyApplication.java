aa2a72a5fec8b5e9e26b106e604264a4f7119c46
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-05-04 21:23:37 -0300

    Initial commit

---
+++ src/main/java/org/cryptolullaby/CryptoLullabyApplication.java
@@ -0,0 +1,13 @@
+package org.cryptolullaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoLullabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoLullabyApplication.class, args);
+	}
+
+}
