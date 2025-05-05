ba8a66577edfae04e79993f09a6ad0f38b1721a3
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-05-05 12:52:58 -0300

    feat: creating entities

--- src/main/java/org/cryptolullaby/entity/Interest.java
+++ src/main/java/org/cryptolullaby/entity/Interest.java
@@ -1,9 +1,39 @@
 package org.cryptolullaby.entity;
 
+import org.cryptolullaby.model.enums.InterestEnum;
+
 public class Interest {
 
     private String id;
 
+    private InterestEnum type;
 
+    public Interest () {}
+
+    public Interest (String id, InterestEnum type) {
+
+        this.id = id;
+
+        this.type = type;
+
+    }
+
+    public String getId () {
+
+        return id;
+
+    }
+
+    public InterestEnum getType () {
+
+        return type;
+
+    }
+
+    public void setType (InterestEnum type) {
+
+        this.type = type;
+
+    }
 
 }
